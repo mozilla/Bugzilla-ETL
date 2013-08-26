cef2ecfdd1d9c7cd4dee5abccfffe969ebfde7da
Kyle Lahnakoski <klahnakoski@mozilla.com>
2013-08-26 13:31:51 -0400

    mostly working, need to profile becasue way too slow

--- src/python/bz_etl.py
+++ src/python/bz_etl.py
@@ -1,17 +1,14 @@
 from datetime import datetime
+import functools
 from util.files import File
+from util.startup import startup
 from util.strings import expand_template
-from util.threads import Queue
-
-from util.threads import Thread
+from util.threads import Queue, Thread
 from util.cnv import CNV
 from util.elasticsearch import ElasticSearch
-from util.struct import Struct
 from util.multithread import Multithread
 from parse_bug_history import parse_bug_history_
 from util.query import Q
-from util.startup import startup
-from util.debug import D
 from util.db import DB, SQL
 from extract_bugzilla import *
 
@@ -32,45 +29,58 @@ def etl(db, es, param):
         get_duplicates
     ]
     #GIVE THEM ALL THE SAME PARAMETERS
+    output=[]
     with Multithread(funcs) as multi:
-        params=[{"db":DB(db), "param":param} for i in range(0, len(funcs))]
+        funcs=[functools.partial(f, {"db":DB(db)}) for f in funcs] #BIND FUNCTION TO DB INSTANCE
+        params=[{"param":param} for f in funcs]
         responses=multi.execute(params)
 
-    #CONCAT ALL RESPONSES
-    output=[]
-    for r in responses:
-        output.extend(r)
+        #CONCAT ALL RESPONSES (BLOCKS UNTIL ALL RIeTRIV)
+        for r in responses:
+            output.extend(r)
+
+    output_queue=Queue()
+    sorted=Q.sort(output, ["bug_id", "_merge_order", {"field":"modified_ts", "sort":-1}])
 
     #USE SEPARATE THREAD TO SORT AND PROCESS BUG CHANGE RECORDS
-    output_queue=Queue()
     process=parse_bug_history_(param, output_queue)
-    def func():
-        sorted=Q.sort(output, ["bug_id", "_merge_order", {"field":"modified_ts", "sort":-1}])
-#        for s in sorted:
-#            if s.bug_id==1883:
-#                D.println("${bug_id}: ${row}", {"bug_id":s.bug_id, "row":s})
-        for s in sorted: process.processRow(s)
-        process.processRow(Struct(bug_id=999999999, _merge_order=1))
-        output_queue.close()
-    Thread.run(func)
+    for s in sorted:
+        process.processRow(s)
+    process.processRow(Struct(bug_id=999999999, _merge_order=1))
+    output_queue.add(Thread.STOP)
 
+    #USE MAIN THREAD TO SEND TO ES
     #output_queue IS A MULTI-THREADED QUEUE, SO THIS WILL BLOCK UNTIL THE 10K ARE READY
     for i, g in Q.groupby(output_queue, size=10000):
         es.add({"id":x._id, "value":x} for x in g)
 
+    return "done"
 
 
 
+#def test(settings):
+#    funcs=[etl for i in range(8)]  #USE ALL 8 PROCESSORS
+#
+#    queue=Multiprocess.Queue()
+#    with Multiprocess(queue, funcs) as multi
+#        for b in range(settings.param.start, settings.param.end, settings.param.increment):
+#            param.BUG_IDS_PARTITION=SQL(expand_template("(bug_id>=${min} and bug_id<${max})", {
+#                "min":b,
+#                "max":b+settings.param.increment
+#            }))
+#            multi.execute(param)
+#
+#    multi.join()
+#
+#
 
-def main():
+def main(settings):
 
-    settings=startup.read_settings()
-    D.settings(settings.debug)
     settings.bugzilla.debug=True
 
     #MAKE HANDLES TO CONTAINERS
     db=DB(settings.bugzilla)
-    es=ElasticSearch(settings.es)
+#    es=ElasticSearch(settings.es)
     if settings.es.alias is None:
         settings.es.alias=settings.es.index
         settings.es.index=settings.es.alias+CNV.datetime2string(datetime.utcnow(), "%Y%m%d_%H%M%S")
@@ -107,17 +117,27 @@ def main():
     param.alias_file=settings.param.alias_file
 
     #
-    max_id=db.query("SELECT max(bug_id) bug_id FROM bugs")[0].bug_id
-    for b in range(settings.param.start, max_id, settings.param.increment):
+    param.end=db.query("SELECT max(bug_id)+1 bug_id FROM bugs")[0].bug_id
+    for b in range(settings.param.start, param.end, settings.param.increment):
         param.BUG_IDS_PARTITION=SQL(expand_template("(bug_id>=${min} and bug_id<${max})", {
             "min":b,
             "max":b+settings.param.increment
         }))
         etl(db, es, param)
+        break
 
 
 
-D.println(CNV.object2JSON([None]))
-main()
+import profile
+
+#threading.setprofile(functools.partial(profile.Profile.trace_dispatch, profile.Profile()))
+
+
+profile.run("""
+settings=startup.read_settings()
+D.start(settings.debug)
+main(settings)
+D.stop()
+""")
 
 
