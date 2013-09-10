8a13f1f780104a1abe48b738150416f9d70dd845
Kyle Lahnakoski <klahnakoski@mozilla.com>
2013-09-10 10:47:45 -0400

    better messages during replication

--- tests/test_replicate.py
+++ tests/test_replicate.py
@@ -6,27 +6,6 @@ from bzETL.util.elasticsearch import ElasticSearch
 from bzETL.util.logs import Log
 
 
-#def get_bug_versions(es, bug_id):
-#    try:
-#        results=es.search({
-#            "query":{"filtered":{
-#                "query":{"match_all":{}},
-#                "filter":{"term":{"bug_id":bug_id}}
-#            }},
-#            "from":0,
-#            "size":200000,
-#            "sort":[]
-#        })
-#
-#        if len(results.hits.hits)==0: return [];
-#        return [b._source for b in results.hits.hits]
-#    except Exception, e:
-#        Log.error("Can not {{bug_id}}", {"bug_id":bug_id}, e)
-#
-#
-#
-
-
 def test_replication():
     try:
         settings=startup.read_settings(filename="replication_settings.json")
@@ -41,12 +20,4 @@ def test_replication():
 
 
 
-
-
-
-
-
-
-
-if __name__=="__main__":
-    test_replication()
+test_replication()
