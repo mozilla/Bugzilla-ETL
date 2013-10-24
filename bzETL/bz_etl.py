dbadd7ecff4a1680a31f9b52d49a16483bb3fadd
Kyle Lahnakoski <klahnakoski@mozilla.com>
2013-10-24 04:01:34 -0400

    fix comments, less 'art'

--- bzETL/bz_etl.py
+++ bzETL/bz_etl.py
@@ -1,12 +1,12 @@
-################################################################################
-## This Source Code Form is subject to the terms of the Mozilla Public
-## License, v. 2.0. If a copy of the MPL was not distributed with this file,
-## You can obtain one at http://mozilla.org/MPL/2.0/.
-################################################################################
-## Author: Kyle Lahnakoski (kyle@lahnakoski.com)
-################################################################################
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Author: Kyle Lahnakoski (kyle@lahnakoski.com)
+#
 
-## REPLACES THE KETTLE FLOW CONTROL PROGRAM, AND BASH SCRIPT
+# REPLACES THE KETTLE FLOW CONTROL PROGRAM, AND BASH SCRIPT
 
 
 from datetime import datetime, timedelta
--- bzETL/util/stats.py
+++ bzETL/util/stats.py
@@ -1,10 +1,10 @@
-################################################################################
-## This Source Code Form is subject to the terms of the Mozilla Public
-## License, v. 2.0. If a copy of the MPL was not distributed with this file,
-## You can obtain one at http://mozilla.org/MPL/2.0/.
-################################################################################
-## Author: Kyle Lahnakoski (kyle@lahnakoski.com)
-################################################################################
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Author: Kyle Lahnakoski (kyle@lahnakoski.com)
+#
 
 from math import sqrt
 from .struct import nvl
