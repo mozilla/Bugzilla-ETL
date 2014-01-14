390c405716c6415af6caf1b13188a131335594a7
Kyle Lahnakoski <klahnakoski@mozilla.com>
2014-01-14 15:41:45 -0500

    look_for_leaks->leak_check

--- tests/resources/scripts/leak_check.bat
+++ tests/resources/scripts/leak_check.bat
@@ -1,2 +1,2 @@
 SET PYTHONPATH=.
-pypy .\tests\resources\python\look_for_leaks.py --settings=leak_check_settings.json
+pypy .\tests\resources\python\leak_check.py --settings=leak_check_settings.json
