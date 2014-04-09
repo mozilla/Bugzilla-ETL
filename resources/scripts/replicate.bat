8b9d92f3616c5069729d5b67a61fa25207c34ed9
Kyle Lahnakoski <klahnakoski@mozilla.com>
2014-04-08 22:22:05 -0400

    include settings to replicate comments

--- resources/scripts/replicate.bat
+++ resources/scripts/replicate.bat
@@ -1,2 +1,3 @@
 SET PYTHONPATH=.
-CALL pypy bzETL\replicate.py --settings=replication_settings.json
+CALL pypy bzETL\replicate.py --settings=resources/scripts/replicate_bugs_settings.json
+CALL pypy bzETL\replicate.py --settings=resources/scripts/replicate_comments_settings.json
