2827ac669e001dc023847c8a865e047b4559316e
Kyle Lahnakoski <klahnakoski@mozilla.com>
2018-03-26 10:03:14 -0400

    script to port forward to distant mysql instance

---
+++ resources/scripts/forward_mysql.bat
@@ -0,0 +1,5 @@
+REM -N DO NOT START SHELL
+REM -v VERBOSE
+REM -L <local_port>:<distant_host>:<distant_port> <putty config for remote>
+
+plink -v -N -L 3307:bugzilla-masterdb-devsvcdev-2017100901.czvlmp16hwe4.us-west-2.rds.amazonaws.com:3306 CloudOps-ETL
