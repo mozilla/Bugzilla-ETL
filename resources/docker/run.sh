e09bd61dba7cfcc84fe3ee0c07a2244be14ff0ec
Kyle Lahnakoski <klahnakoski@mozilla.com>
2018-08-09 07:35:46 -0400

    fix script

--- resources/docker/run.sh
+++ resources/docker/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # RUN FROM ROOT Bugzilla-ETL DIRECTORY, eg ./resources/docker/build.sh
-docker run --user app --env-file ./resources/docker/public_etl.env --mount source=public_etl_state,destination=/app/logs bugzilla-etl bash
+docker run --user app --env-file ./resources/docker/public_etl.env --mount source=public_etl_state,destination=/app/logs bugzilla-etl
